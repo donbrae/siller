73a33b3d0ad17ff3c848734a0c987ec5d5604a6d
donbrae <jamie@donbrae.co.uk>
2020-08-07 12:11:39 +0100

    Improved basic functionality.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,31 @@ import Field from "./components/Field";
 import './App.css';
 
 function App() {
-    const [account, setAccount] = useState('account');
-    const [perday, setPerday] = useState('perday');
+    const [account, setAccount] = useState(0);
+    const [perday, setPerday] = useState(0);
+    const [calcul, setCalcul] = useState('x');
+
+    function goCalculate() {
+
+        if (parseFloat(account) && parseFloat(perday)) {
+            setCalcul((parseFloat(account) / parseFloat(perday)).toFixed(2));
+        } else {
+          setCalcul('x');
+        }
+    }
 
     useEffect(() => {
-        console.log(perday);
 
+        goCalculate();
         return() => {
             console.log('optional cleanup function');
         }
-    }, []); // Runs on init and when value of 'perday' changes
+    }); // Runs on init and every update
 
-    return (<div><Field id="account" name="Bank account balance" func={(e) => setAccount(e.target.value)}/><Field id="perday" name="Days left till payday" func={(e) => setPerday(e.target.value)}/>
-        <h2>You can spend £{account / perday}   per day</h2>
+    return (<div>
+        <Field id="account" value={account} name="Bank account balance £" func={(e) => setAccount(e.target.value)}/>
+        <Field id="perday" value={perday} name="Days left till payday" func={(e) => setPerday(e.target.value)}/>
+        <h2>You can spend £{calcul} per day before you’re next paid.</h2>
     </div>);
 }
 
--- src/components/Field.js
+++ src/components/Field.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Field(props) {
     return (<div>
-        <label for={props.id}>{props.name}</label>
-        £<input type="text" id={props.id} onChange={props.func}></input>
+        <label htmlFor={props.id}>{props.name}</label>
+        <input type="text" id={props.id} value={props.value} onChange={props.func}></input>
     </div>);
 }
 
