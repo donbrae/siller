ebcd193fa875037d05f8d60b6c94149305206d6d
donbrae <jamie@donbrae.co.uk>
2020-08-09 11:25:35 +0100

    Added useCallback() to Field so that pending transaction fields are focused on create.

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 
 function Field(props) {
 
+    const afterCreate = useCallback(node => {
+
+        // 'Pending' transation fields
+        if (node && props.class.includes('pending') && props.dataId > 2) { // > Can we dynamically get the value of pending.length instead of using '2'?
+            node.focus();
+        }
+    }, []);
+
     return (<div className="form-group">
-    <label htmlFor={props.id}>{props.name}</label>
+        <label htmlFor={props.id}>{props.name}</label>
         <div className="input-group">
-            <input type="text" className={props.class} id={props.id} data-id={props.dataId} value={props.value} onChange={props.handler}></input>
+            <input type="text" className={props.class} id={props.id} data-id={props.dataId} value={props.value} ref={afterCreate} onChange={props.handler}></input>
         </div>
     </div>);
 }
